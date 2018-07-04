f8f0496cbe5ca2ab2a41003d58578660795e7b90
mon823 <33455696+mon823@users.noreply.github.com>
2018-07-04 13:29:32 +0900

    Insertion_Sort_2

---
+++ Sort/Insertion_Sort/Insertion_Sort_2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+void insertionSort(int *arr, int n);
+
+int main(void) {
+	int n, arr[10000];
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+		scanf("%d", &arr[i]);
+
+	insertionSort(arr, n);
+
+	for (int i = 0; i<n; i++)
+		printf("%d\n", arr[i]);
+}
+
+void insertionSort(int *arr, int n) {
+	int k;
+	int loc;
+	int newItem;
+	for (int i = 2; i < n; i++) {
+		loc = i - 1;
+		newItem = arr[i];
+		while (loc >= 1 && newItem < arr[loc]) {
+			arr[loc + 1] = arr[loc];
+			loc = loc - 1;
+		}
+		arr[loc + 1] = newItem;
+	}
+}
